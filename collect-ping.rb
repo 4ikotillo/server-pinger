d6d9298f7b50c6dd06d5728a9893c16e6822f219
4ikotillo <crazyspbboy@gmail.com>
2019-12-07 00:51:43 +0100

    remove ms

--- collect-ping.rb
+++ collect-ping.rb
@@ -13,5 +13,5 @@ result = CSV.open("./results.csv", "w", col_sep: ";")
 results = csv.map do |line|
   ping_result = `ping #{address} -c #{5}`
   average_response_time = ping_result.scan(/min\/avg\/max\/stddev\s\=\s\d+\.\d+\/(\d+\.\d+)\//).flatten[0].to_f
-  result << [line[0], "#{average_response_time} ms"]
+  result << [line[0], "#{average_response_time}"]
 end
